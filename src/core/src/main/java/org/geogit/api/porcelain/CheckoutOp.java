54c0a7cc0eaed93ac4a6e5d6c700ab9ef0b38eca
Johnathan Garrett <johnathan.garrett@lmnsolutions.com>
2014-01-06 15:08:45 -0500

    Fixed some issues with handling deleted features while resolving merge conflicts.

--- src/core/src/main/java/org/geogit/api/porcelain/CheckoutOp.java
+++ src/core/src/main/java/org/geogit/api/porcelain/CheckoutOp.java
@@ -163,12 +163,7 @@ public class CheckoutOp extends AbstractGeoGitOp<CheckoutResult> {
 
                 if ((ours || theirs) && !node.isPresent()) {
                     // remove the node.
-                    RevTreeBuilder treeBuilder = new RevTreeBuilder(getIndex().getDatabase(),
-                            getWorkTree().getTree());
-                    treeBuilder.remove(st);
-                    RevTree newRoot = treeBuilder.build();
-                    getIndex().getDatabase().put(newRoot);
-                    getWorkTree().updateWorkHead(newRoot.getId());
+                    command(RemoveOp.class).addPathToRemove(st).call();
                 } else {
                     checkState(node.isPresent(), "pathspec '" + st
                             + "' didn't match a feature in the tree");
